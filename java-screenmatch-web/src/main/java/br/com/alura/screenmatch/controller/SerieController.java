87fe82cfa1429b9895bbcea5163fcae2b6482041
witalomelo <witalovmelo@gmail.com>
2024-05-12 16:52:59 -0300

    ajustando aplicacao para API

---
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+
+    @GetMapping("/series")
+    public String obterSeries(){
+        return "mostrando series";
+
+    }
+}
